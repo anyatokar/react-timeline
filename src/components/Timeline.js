85f481fe375b463d1ee0890a38968c8837858ffd
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-12-27 21:40:23 -0500

    renders person and status but not date

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,20 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
+const Timeline = (props) => {
+
+  const timelineEvents = props.events.map((event, i) => {
+    return (
+        <TimelineEvent key={i} person={event.person} status={event.status} timeStamp={event.timeStamp}/>
+    );
+  });
+
+  return (
+    <div className="timeline">
+      {timelineEvents}
+    </div>
+  );
   
-  return;
 }
 
 export default Timeline;
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+
+const TimelineEvent = (props) => {
+  return (
+  <div className='timeline-event'>
+    <p className='event-person'>{props.person} </p> 
+    <p className='event-status'>{props.status}</p>
+    <p className='event-time'><Timestamp timeStamp={props.timeStamp} /></p>
+  </div>
+  )
 }
 
 export default TimelineEvent;
